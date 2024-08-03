8b3beca4c4526739423ce25d54ad1b7a9dd5d81a
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-08-03 12:18:21 +0600

    State Updated (Temporary)

--- src/store.js
+++ src/store.js
@@ -1,6 +1,9 @@
 import { create } from 'zustand'
 
 const useStore = create((set) => ({
+    isOpen: false,
+    openModal: () => set({ isOpen: true }),
+    closeModal: () => set({ isOpen: false }),
     selectedAnimation: 'tsh-3d-float',
     setSelectedAnimation: (animation) => set({ selectedAnimation: animation }),
     backgroundColor: '#ffffff',
