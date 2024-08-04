e39c85b7abc5331429d7ea2d55f99f6800968aee
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-08-04 22:45:45 +0600

    Updated

--- src/frontend/components/Container.jsx
+++ src/frontend/components/Container.jsx
@@ -5,23 +5,29 @@ import CardLayout from './layouts/card';
 
 function Container() {
   const editorPanelRef = useRef(null);
+  const editorContainerRef = useRef(null);
 
   useEffect(() => {
     const currentPath = window.location.pathname;
     const editorPanel = editorPanelRef.current;
+    const editorContainer = editorContainerRef.current;
     if (currentPath === '/editor' && editorPanel && editorPanel.classList.contains('tsteam-container')) {
       editorPanel.id = 'editorPanel';
       editorPanel.classList.add('editor-hover');
+      editorContainer.classList.add('editor-container');
     }
   }, []);
 
   return (
     <>
-      <div className="flex justify-center">
+      <div 
+      ref={editorContainerRef}
+      className="">
         <div
           ref={editorPanelRef}
           className="tsteam-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-3/6"
         >
+          <CardLayout />
           <CardLayout />
         </div>
       </div>
