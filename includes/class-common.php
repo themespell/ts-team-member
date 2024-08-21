c701447a9eea089e63d15ca07ffb525820e66101
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-08-21 11:09:36 +0600

    Added Current Screen Info

--- includes/class-common.php
+++ includes/class-common.php
@@ -19,10 +19,21 @@ if ( ! defined( 'ABSPATH' ) ) {
  * This class handles common methods.
  */
 class Common {
+
     public static function isProActivated() {
         include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
         $plugin_file = 'tsteam-pro/class-tsteam-pro.php';
         return is_plugin_active($plugin_file);
+    }
+
+    public static function get_current_screen_info(){
+        $current_screen  = get_current_screen();
+        $targeted_screen = 'tsteam-showcase';
+
+        if ( $targeted_screen === $current_screen->post_type ) {
+            return true;
+        }
+        return false;
     }
 
 }
