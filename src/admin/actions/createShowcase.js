3f0ce5bdd8f085a1fa6b24a3e58c15f9e219dc4e
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-08-25 21:42:10 +0600

    Create Showcase Function Improved

--- src/admin/actions/createShowcase.js
+++ src/admin/actions/createShowcase.js
@@ -1,17 +1,17 @@
 const ajax_url = tsteam_settings.ajax_url;
-const admin_url = tsteam_settings.admin_url;
 
-export const createShowcase = ( data ) => {
-    jQuery.post(ajax_url, {
-      _ajax_nonce: tsteam_settings.nonce,
-      action: "tsteam_showcase/create_showcase",
-      title: data.title,
-      }, function(response) {
-        if (response.success) {
-            // window.location.href = `${admin_url}edit.php?post_type=tsteam-showcase&path=admin`
-          } else {
-            console.log('Error:', response);
-          }
-      }
-    );
-}
+export const createShowcase = (data) => {
+    return new Promise((resolve, reject) => {
+        jQuery.post(ajax_url, {
+            _ajax_nonce: tsteam_settings.nonce,
+            action: "tsteam_showcase/create_showcase",
+            title: data.title,
+        }, function(response) {
+            if (response.success) {
+                resolve(response);  // Resolve the promise on success
+            } else {
+                reject(response);  // Reject the promise on failure
+            }
+        });
+    });
+};
