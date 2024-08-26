167b77ed29a290c51eb5f92be66557a3627a7104
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-08-26 11:06:35 +0600

    Added Admin Class

---
+++ includes/class-admin.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace TSTeam;
+use TSTeam\Common;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class Admin {
+
+	public static function init() {
+		$self = new self();
+		add_action( 'admin_menu', array( $self, 'add_admin_menu' ) );
+	}
+
+    public function add_admin_menu() {
+		$parent = 'tsteam-admin';
+
+		add_menu_page(
+			__( 'TS Team', 'ts-team' ),
+			'TS Team',
+			'do_not_allow',
+			$parent,
+			array( $this, 'tsteam_callback' ),
+			plugin_dir_url( __FILE__ ) . 'assets/icon-16x16.png',
+			30
+		);
+		add_submenu_page(
+			$parent,
+			__( 'Team Showcase', 'ts-team' ),
+			__( 'Team Showcase', 'ts-team' ),
+			'manage_options',
+			'tsteam-showcase',
+			array( $this, 'tsteam_callback' ),
+		);
+	}
+
+	public function tsteam_callback() {
+		?>
+		<div id="tsteam-admin"></div>
+		<?php
+	}
+}
