77b368f97a624f09f8ffebc48eeb228bb8f8f5bc
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-08-26 11:07:50 +0600

    Editor Condition Updated & Loader Added

--- src/editor/Editor.jsx
+++ src/editor/Editor.jsx
@@ -1,13 +1,27 @@
 import { useState, useEffect } from 'react';
 import useStore from '../store.js';
+import { Spin } from 'antd';
 import Topbar from './components/Topbar.jsx';
 import Sidebar from './components/Sidebar.jsx';
 import Frontend from '../frontend/Frontend.jsx';
+import { hideAdminElements } from './utils/utils.js';
 
 function Editor() {
   const { selectedAnimation } = useStore();
   const [theme, setTheme] = useState('Theme One'); // New state for theme selection
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false); // State to manage sidebar visibility
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true); // State to manage sidebar visibility
+  const [isLoading, setIsLoading] = useState(true); // State for loading spinner
+
+  useEffect(() => {
+    hideAdminElements();
+
+    // Simulate a loading delay of up to 2 seconds
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 100); // 2 seconds
+
+    return () => clearTimeout(timer); // Cleanup timer on component unmount
+  }, []);
 
   useEffect(() => {
     const handleClick = () => setIsSidebarOpen(true);
@@ -25,6 +39,17 @@ function Editor() {
   const handleCloseSidebar = () => {
     setIsSidebarOpen(false);
   };
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen bg-blue-700">
+        <Spin
+        fullscreen 
+        tip="Loading Editor"
+        size="large" />
+      </div>
+    );
+  }
 
   return (
     <>
