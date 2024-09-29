e6bb231bdd17660baf4f223d70218fdba345249c
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-09-29 11:13:17 +0600

    Updated

--- src/frontend/Frontend.jsx
+++ src/frontend/Frontend.jsx
@@ -1,12 +1,21 @@
-import useStore from '../store.js';
-import Container from './components/Container.jsx';
+import { useEffect, useRef } from 'react';
+import './assets/hover.css';
+import './assets/style.css';
+import Layout from './components/Layout.jsx';
 
-function Frontend() {
-  const { selectedAnimation } = useStore();
-
+function Frontend({ layout }) {
   return (
     <>
-        <Container />
+        <div className="tsteam-container grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 w-3/6">
+          <Layout
+              layoutType={layout}
+              imageUrl="https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/01/team-img-28.jpg"
+              title="John Doe"
+              subtitle="Managing Director"
+              description="Experienced leader in managing teams across various industries."
+              socialIcons={["<i class='fab fa-facebook'></i>", "<i class='fab fa-twitter'></i>"]}
+          />
+        </div>
     </>
   );
 }
