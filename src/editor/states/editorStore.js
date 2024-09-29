9ac14733290b5c32a60c7e49109c0b27df0f3ace
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-09-29 11:15:07 +0600

    Editor State Store Updated

--- src/editor/states/editorStore.js
+++ src/editor/states/editorStore.js
@@ -1,7 +1,9 @@
 import { create } from 'zustand'
 
 const editorStore = create((set) => ({
-    
+    // Content Tab
+    layout: 'Card',
+    setLayout: (selectedLayout) => set({ layout: selectedLayout }),
 }))
 
 export default editorStore;
