8c256ef9518333cee50f573f98d25e2b4af000a5
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-09-29 11:14:44 +0600

    Added Global Settings

---
+++ src/common/utils/globalSettings.js
@@ -0,0 +1,14 @@
+const globalSettings = {
+    token: {
+        colorPrimary: '#00b96b',
+        colorBgContainer: '#fff',
+        colorText: '#000000',
+    },
+    theme: {
+      primaryColor: 'red',
+      textColor: '#333',
+      borderColor: '#ededed'
+    },
+  };
+  
+export default globalSettings;  
