11e83a6576b9f9818d8e4763fed077f0ca48176e
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-02 12:47:54 +0600

    Updated Store

--- src/editor/states/editorStore.js
+++ src/editor/states/editorStore.js
@@ -3,9 +3,11 @@ import { create } from 'zustand'
 const editorStore = create((set) => ({
     // Variables
     layout: 'Card',
+    view: 'grid',
 
     // Functions
     setLayout: (selectedLayout) => set({ layout: selectedLayout }),
+    setView: (selectedView) => set({ view: selectedView }),
 }))
 
 export default editorStore;
