703d03574e8f77883c6b366d0a4870bbddcb01a8
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-02 12:10:13 +0600

    Added Editor Props Support

--- src/common/components/Container.jsx
+++ src/common/components/Container.jsx
@@ -1,11 +1,11 @@
 import TableNav from "./TableNav";
 import DataTable from "./DataTable";
 
-function Container({ type, title }) {
+function Container({ type, title, editor }) {
   return (
     <>
       <TableNav type={type} title={title} />
-      <DataTable type={type} title={title} />
+      <DataTable type={type} title={title} editor={editor} />
     </>
   );
 }
