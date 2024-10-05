d28ff03f835bcc6d2f2d015c88e523a78c27b474
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-05 23:28:12 +0600

    Updated

--- src/common/components/CrudModal.jsx
+++ src/common/components/CrudModal.jsx
@@ -1,22 +1,23 @@
 import FormContainer from './helper/form/FormContainer.jsx';
 import useAdminStore from '../../admin/states/admin-store.js';
-import { Modal } from 'antd';
+import { TsModal } from './controls/tsControls.js';
 
-function CrudModal({type, name}) {
+function CrudModal({actionType, type, name, id}) {
   const { isOpen, openModal, closeShowcaseModal } = useAdminStore();
-  const modalTitle = `Add New ${name}`;
   return (
     <>
-     <Modal
-                title={modalTitle} 
-                open={isOpen} 
-                onOk={closeShowcaseModal} 
-                onCancel={closeShowcaseModal}
-                width={700}
-                footer={[]}
-              >
-              <FormContainer name={name} type={type} />
-       </Modal>
+    <TsModal
+    actionType={actionType}
+    name={name}
+    isOpen={isOpen}
+    onClose={closeShowcaseModal}
+    width={550}>
+          <FormContainer
+          actionType={actionType}
+          name={name} 
+          type={type} 
+          />
+    </TsModal>
     </>
   );
 }
