5f27b94e15ab59aa9c74cc39cc8ac6291fa0278b
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-05 23:28:07 +0600

    Added TsModal Control

---
+++ src/common/components/controls/TsModal.jsx
@@ -0,0 +1,29 @@
+import { Modal } from 'antd';
+import FormContainer from '../helper/form/FormContainer';
+
+function TsModal({ actionType, formSupport, name, type, id, isOpen, isClose, width, children }) {
+    console.log(id);
+  return (
+    <Modal
+      title={
+        actionType === 'create' 
+          ? `Add New ${name}` 
+          : actionType === 'edit' 
+          ? `Edit ${name}` 
+          : actionType === 'delete' 
+          ? `Delete ${name}` 
+          : ''
+      }
+      open={isOpen}
+      onCancel={isClose}
+      footer={null}
+      closable={true}
+      centered={true}
+      width={width}
+    >
+      {formSupport ? <FormContainer actionType={actionType} name={name} type={type} post_id={id} /> : children}
+    </Modal>
+  );
+}
+
+export default TsModal;
