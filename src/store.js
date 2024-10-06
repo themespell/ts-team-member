f3007ce7eb707998b55dc6a0bc0776b2ac50ba8d
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-06 23:17:01 +0600

    Updated

--- src/store.js
+++ src/store.js
@@ -4,12 +4,6 @@ const useStore = create((set) => ({
     isOpen: false,
     openModal: () => set({ isOpen: true }),
     closeModal: () => set({ isOpen: false }),
-    selectedAnimation: 'tsh-3d-float',
-    setSelectedAnimation: (animation) => set({ selectedAnimation: animation }),
-    backgroundColor: '#ffffff',
-    setBackgroundColor: (color) => set({ backgroundColor: color }),
-    postId: null,
-    setPostId: (id) => set({ postId: id }),
 }))
 
 export default useStore;
