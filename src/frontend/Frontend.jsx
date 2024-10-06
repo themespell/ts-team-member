6a30d2e21a6a0e183c94aba3dd209c770353738c
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-06 23:17:20 +0600

    Frontend Updated

--- src/frontend/Frontend.jsx
+++ src/frontend/Frontend.jsx
@@ -1,55 +1,64 @@
-import { useEffect, useRef } from 'react';
+import { StrictMode, useEffect, useState } from 'react';
+import { createRoot } from 'react-dom/client';
 import './assets/hover.css';
 import './assets/style.css';
-import Layout from './components/Layout.jsx';
-import { Carousel } from "antd";
+import StaticView from './components/StaticView.jsx';
+import CarouselView from './components/CarouselView.jsx';
+import { fetchData } from '../common/services/fetchData.js';
 
-const contentStyle = {
-  margin: 0,
-  height: '160px',
-  color: '#fff',
-  textAlign: 'center',
-  background: '#364d79',
-};
+const showcaseElements = document.querySelectorAll('.tsteam-showcase');
 
-function Frontend({ layout, view, data }) {
-  const team_members = data.team_members;
+showcaseElements.forEach(element => {
+  const id = element.getAttribute('data-id');
+  const layoutType = element.getAttribute('data-layout-type');
 
-  const onChange = (currentSlide) => {
-    console.log(currentSlide);
-  };
+  createRoot(element).render(
+    <StrictMode>
+      <Frontend
+        layout={layoutType || 'Overlay'}
+        id={id}
+      />
+    </StrictMode>
+  );
+});
+
+function Frontend({ layout, view, id, data }) {
+  const [team_members, setTeamMembers] = useState(data?.team_members || null);
+
+  useEffect(() => {
+    if (!team_members && id) {
+      fetchData(`tsteam/team_showcase/fetch/single`, (response) => {
+        if (response && response.success) {
+          setTeamMembers(response.data.meta_data.team_members);
+        } else {
+          console.error("Error fetching post data:", response);
+        }
+      }, { post_id: id });
+    } else if (!id) {
+      console.error("No post_id found");
+    }
+  },);
 
   return (
     <>
       {view === "carousel" ? (
-        <div className='tsteam-container w-3/6'>
-            <Carousel slidesPerRow={2} slidesToScroll={1} draggable centerMode autoplay afterChange={onChange}>
-            {team_members && team_members.length > 0 ? (
-              team_members.map((member, index) => (
-                <div key={index}>
-                  <Layout
-                    layoutType={layout}
-                    imageUrl={member.team_member_image || "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/01/team-img-28.jpg"} // Fallback image if none provided
-                    title={member.title || "No Name"}
-                    subtitle={member.subtitle || "No Subtitle"}
-                    description={member.description || "No description available."}
-                    socialIcons={member.socialIcons || []}
-                  />
-                </div>
-              ))
-            ) : (
-              <p>No team members found.</p>
-            )}
-          </Carousel>
-        </div>
+        <CarouselView
+          team_members={team_members}
+          layout={layout}
+        />
       ) : (
-        <div className="tsteam-container grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 w-3/6">
+        <div 
+        // style={{
+        //   gridTemplateColumns: 'repeat(4, 1fr)',
+        //   gap: '50px',             // Adjust gap between grid items (optional)
+        // }}
+        className="tsteam-container">
           {team_members && team_members.length > 0 ? (
             team_members.map((member, index) => (
-              <Layout
+              <StaticView
                 key={index}
                 layoutType={layout}
-                imageUrl={member.team_member_image || "https://qodeinteractive.com/qi-addons-for-elementor/wp-content/uploads/2021/01/team-img-28.jpg"} // Fallback image if none provided
+                imageUrl={member.team_member_image}
                 title={member.title || "No Name"}
                 subtitle={member.subtitle || "No Subtitle"}
                 description={member.description || "No description available."}
