f640c09ff972936d9607bc95fb272f1bfee3321c
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-08 09:53:58 +0600

    Frontend Updated

--- src/frontend/Frontend.jsx
+++ src/frontend/Frontend.jsx
@@ -10,65 +10,48 @@ const showcaseElements = document.querySelectorAll('.tsteam-showcase');
 
 showcaseElements.forEach(element => {
   const id = element.getAttribute('data-id');
-  const layoutType = element.getAttribute('data-layout-type');
 
   createRoot(element).render(
     <StrictMode>
       <Frontend
-        layout={layoutType || 'Overlay'}
         id={id}
       />
     </StrictMode>
   );
 });
 
-function Frontend({ layout, view, id, data }) {
-  const [team_members, setTeamMembers] = useState(data?.team_members || null);
+function Frontend({ id }) {
+  const [teamMembers, setTeamMembers] = useState([]);
+  const [settings, setSettings] = useState({});
 
   useEffect(() => {
-    if (!team_members && id) {
+    if (id) {
       fetchData(`tsteam/team_showcase/fetch/single`, (response) => {
         if (response && response.success) {
           setTeamMembers(response.data.meta_data.team_members);
+          const showcaseSettings = JSON.parse(response.data.meta_data.showcase_settings);
+          setSettings(showcaseSettings);
         } else {
           console.error("Error fetching post data:", response);
         }
       }, { post_id: id });
-    } else if (!id) {
+    } else {
       console.error("No post_id found");
     }
-  },);
+  }, [id]);
 
   return (
     <>
-      {view === "carousel" ? (
+      {settings.view === "carousel" ? (
         <CarouselView
-          team_members={team_members}
-          layout={layout}
+          team_members={teamMembers}
+          settings={settings}
         />
       ) : (
-        <div 
-        // style={{
-        //   gridTemplateColumns: 'repeat(4, 1fr)',
-        //   gap: '50px',             // Adjust gap between grid items (optional)
-        // }}
-        className="tsteam-container">
-          {team_members && team_members.length > 0 ? (
-            team_members.map((member, index) => (
-              <StaticView
-                key={index}
-                layoutType={layout}
-                imageUrl={member.team_member_image}
-                title={member.title || "No Name"}
-                subtitle={member.subtitle || "No Subtitle"}
-                description={member.description || "No description available."}
-                socialIcons={member.socialIcons || []}
-              />
-            ))
-          ) : (
-            <p>No team members found.</p>
-          )}
-        </div>
+        <StaticView
+          team_members={teamMembers}
+          settings={settings}
+        />
       )}
     </>
   );
