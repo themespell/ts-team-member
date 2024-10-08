043db63b9c37996b5e19cddcfe0a25de77d6c163
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-08 09:54:56 +0600

    Editor Topbar Updated

--- src/editor/components/Topbar.jsx
+++ src/editor/components/Topbar.jsx
@@ -1,13 +1,22 @@
 import { TsButton } from "../../common/components/controls/tsControls";
+import editorFunction from "../states/editorFunction";
 
-function Topbar() {
+function Topbar({ type }) {
+    const handlePublishClick = () => {
+        const action = `tsteam/${type}/update/settings`;
+        editorFunction.getState().updateSettings(action);
+    };
+
     return (
         <>
-        <div class="flex flex-col justify-end bg-black p-4">
-          <TsButton 
-          label="Publish"
-          />
-      </div>
+        <div className="flex justify-end bg-black p-4">
+            <div className="flex justify-end w-2/6">
+                <TsButton 
+                    label="Publish"
+                    onClick={handlePublishClick}
+                />
+            </div>
+        </div>
         </>
     );
 }
