ef21df810fe1db9ca9a6ddf0dcb733d59c6e33cb
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-08 09:55:35 +0600

    Updated Nav Items

--- src/common/components/helper/TableNavItems.jsx
+++ src/common/components/helper/TableNavItems.jsx
@@ -1,12 +1,15 @@
 import useAdminStore from '../../../admin/states/admin-store.js';
+import { TsButton } from '../controls/tsControls.js';
 
 function TableNavItems({title}) {
     const { isOpen, openModal, closeShowcaseModal } = useAdminStore();
     return (
       <div className="flex justify-between">
-        <button onClick={openModal} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-            Create New {title}
-        </button>
+        <TsButton 
+        label={title}
+        prefix='Create'
+        onClick={openModal}
+        />
       </div>                
     );
   }
