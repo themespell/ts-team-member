c9b4c0dd0bac07b9aabf065b9e247e635a4fc27d
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-09 20:36:06 +0600

    Fixed Slider Control Bug

--- src/common/components/controls/TsSlider.jsx
+++ src/common/components/controls/TsSlider.jsx
@@ -2,20 +2,21 @@ import { Slider, InputNumber } from 'antd';
 
 import globalSettings from '../../utils/globalSettings';
 
-function TsSlider({ label, range, value, onChange }) {
+function TsSlider({ label, range, value, unit, onChange }) {
   return (
     <>
     {label && (
-            <label className="block text-sm font-medium text-gray-700 mb-1">
-              {label}
-            </label>
+            <div className='flex justify-between'>
+              <label className="block text-sm font-medium text-gray-700 mb-1">{label}</label>
+              <label className="block text-sm font-medium text-gray-700 mb-1 mr-1">{unit}</label>
+            </div>
       )}
       <div className="mb-4 flex justify-between items-center w-full">
         <div className="w-full mr-6">
           <Slider
-            value={value}
-            min={range.min}
-            max={range.max}
+            value={parseInt(value)}
+            min={parseInt(range.min)}
+            max={parseInt(range.max)}
             onChange={onChange}
             style={{
                 track: {
@@ -27,9 +28,9 @@ function TsSlider({ label, range, value, onChange }) {
         </div>
         <div>
           <InputNumber
-            value={value}
-            min={range.min}
-            max={range.max}
+            value={parseInt(value)}
+            min={parseInt(range.min)}
+            max={parseInt(range.max)}
             onChange={onChange}
           />
         </div>
