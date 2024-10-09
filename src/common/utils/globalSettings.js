425b5252aee846e5249f090b946723e20b2d8dd2
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-09 10:48:46 +0600

    Global Settings Updated

--- src/common/utils/globalSettings.js
+++ src/common/utils/globalSettings.js
@@ -1,14 +1,34 @@
+import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid';
+
 const globalSettings = {
     token: {
-        colorPrimary: '#00b96b',
+        colorPrimary: '#18208D',
         colorBgContainer: '#fff',
         colorText: '#000000',
     },
+    components: {
+      Table: {
+        "headerBg": "#18208D",
+        "headerColor": "#ffffff",
+      }
+    },
     theme: {
-      primaryColor: 'red',
+      primaryColor: '#18208D',
       textColor: '#333',
       borderColor: '#ededed'
     },
+    navigation: {
+      teamShowcase:{
+        link: '?page=tsteam-showcase',
+        label: 'Team Showcase',
+        icon: PhotoIcon
+      },
+      teamMember:{
+        link: '?page=tsteam-showcase&path=team-member',
+        label: 'Team Member',
+        icon: UserCircleIcon
+      }
+    }
   };
   
 export default globalSettings;  
