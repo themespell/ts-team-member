9983db4ce43ece7996a8829ae29941a2fb6ab3d1
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-15 14:41:06 +0600

    Fixed Data Table Column and Loading Issue

--- src/common/components/DataTable.jsx
+++ src/common/components/DataTable.jsx
@@ -6,13 +6,18 @@ import { toastNotification } from '.././utils/toastNotification.js';
 import { TsModal } from './controls/tsControls.js';
 import { PaintBrushIcon, PencilIcon, PencilSquareIcon, TrashIcon } from "@heroicons/react/24/solid";
 
+import { Typography } from 'antd';
+const { Text } = Typography;
+
 function DataTable({ type, title, editor }) {
   const [data, setData] = useState([]);
   const [columns, setColumns] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedPost, setSelectedPost] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     fetchData(`tsteam/${type}/fetch`, (response) => {
       if (response && response.success) {
         const showcaseData = response.data.map((item) => ({
@@ -20,12 +25,25 @@ function DataTable({ type, title, editor }) {
           ...item,
         }));
         
-        const dynamicColumns = Object.keys(showcaseData[0]).map((key) => ({
+        const dynamicColumns = Object.keys(showcaseData[0])
+        .filter((key) => key !== 'post_id' && key !== 'key')
+        .map((key) => ({
           title: key.charAt(0).toUpperCase() + key.slice(1),
           dataIndex: key,
           key: key,
-          render: (text) => <span>{text}</span>,
-        }));
+          render: (text) => {
+            switch (key) {
+              case 'image':
+              case 'profileImage':
+                return <img src={text} alt={key} style={{ width: '70px', height: 'auto', borderRadius: '100%' }} />;
+              case 'shortcode':
+                return <Text copyable>{text}</Text>;
+
+              default:
+                return <span>{text}</span>;
+            }
+          },
+      }));
 
         const actionColumn = {
           title: 'Action',
@@ -46,6 +64,7 @@ function DataTable({ type, title, editor }) {
       } else {
         console.error('Error fetching showcases:', response);
       }
+      setLoading(false);
     });
   }, [type]);
 
@@ -89,7 +108,9 @@ function DataTable({ type, title, editor }) {
     <div className="shadow-md rounded-lg overflow-hidden mt-4">
       <Table
       columns={columns} 
-      dataSource={data} />
+      dataSource={data}
+      loading={loading}
+       />
 
       <TsModal
           actionType='edit'
