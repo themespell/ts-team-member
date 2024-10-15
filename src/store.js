a598d43c21b8b680d8a6f300f4ba60ecbb5df328
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-15 17:18:40 +0600

    Removed

--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand'
-
-const useStore = create((set) => ({
-    isOpen: false,
-    openModal: () => set({ isOpen: true }),
-    closeModal: () => set({ isOpen: false }),
-}))
-
-export default useStore;
