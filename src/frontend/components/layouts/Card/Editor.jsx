659d5f22a75d8936202a85ecc5ac766bc007b37d
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-15 14:35:08 +0600

    Card Layout Updated

--- src/frontend/components/layouts/Card/Editor.jsx
+++ src/frontend/components/layouts/Card/Editor.jsx
@@ -1,22 +1,40 @@
-import editorStore from '../../../../editor/states/editorStore';
-
 const register_controls = () => {
-    const range = {
-        min: 0,
-        max: 100
-    };
     return {
         controls: [
             {
-                type: 'slider',
-                label: 'Slider',
-                name: 'newslider.default',
-                range: range,
-                value: editorStore().newslider?.default || 5,
+                type: 'divider',
+                label: 'Card Styles'
             },
             {
-                type: 'divider',
-            }
+                type: 'color',
+                label: 'Background Color',
+                name: 'tscard.backgroundColor',
+            },
+            {
+                type: 'color',
+                label: 'Text Color',
+                name: 'tscard.textColor',
+            },
+            {
+                type: 'color',
+                label: 'Border Color',
+                name: 'tscard.borderColor',
+            },
+            {
+                type: 'color',
+                label: 'Hover Background Color',
+                name: 'tscard.hoverColor',
+            },
+            {
+                type: 'color',
+                label: 'Hover Text Color',
+                name: 'tscard.hoverTextColor',
+            },
+            {
+                type: 'color',
+                label: 'Hover Border Color',
+                name: 'tscard.hoverBorderColor',
+            },
         ]
     };
 };
