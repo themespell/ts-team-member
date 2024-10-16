577101f5ede82db3f16ca1ac6cad427bce6b7607
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-16 21:09:44 +0600

    Updated TSMedia Control

--- src/common/components/controls/TsMedia.jsx
+++ src/common/components/controls/TsMedia.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react';
 import TsButton from './TsButton';
-import { Button, Image, Form } from 'antd';
+import { Image, Form } from 'antd';
 
-function TsMedia({ label, name, required, form }) {
-  const [mediaUrl, setMediaUrl] = useState(null);
-
+function TsMedia({ label, name, form, mediaUrl, setMediaUrl }) {
   const openMediaLibrary = () => {
     var wkMedia;
 
@@ -24,7 +21,9 @@ function TsMedia({ label, name, required, form }) {
 
     wkMedia.on('select', function () {
       const attachment = wkMedia.state().get('selection').first().toJSON();
-      setMediaUrl(attachment.url);  // Set the media URL to state
+      if (setMediaUrl) {
+        setMediaUrl(attachment.url);  // Set the media URL through the prop function
+      }
       form.setFieldsValue({ [name]: attachment.url }); // Set form field value dynamically using `name`
     });
 
@@ -38,31 +37,18 @@ function TsMedia({ label, name, required, form }) {
         name={name}
         rules={[
             {
-                required: required,  // Fix this line
+                required: true,
                 message: `Please select ${label}!`
             }
         ]}
       >
         <div className='flex items-center gap-6 w-full'>
-          {/* Conditionally render the Image component if mediaUrl is set */}
-          {mediaUrl && (
-            <Image
-              src={mediaUrl}
-              alt="Selected media"
-              style={{ 
-                borderRadius: '100%',
-                height: '10rem',
-                width: '10rem',
-              }}
-            />
-          )}
           <TsButton 
-          label='Select Member Image'
-          id="wk-button"
-          htmlType="button"
-          onClick={openMediaLibrary}
+            label={`Select ${label}`}
+            id="wk-button"
+            htmlType="button"
+            onClick={openMediaLibrary}
           />
-
         </div>
       </Form.Item>
     </>
