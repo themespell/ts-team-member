e4f0c370631e43dd680241d8170b30fcdc7078bd
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-21 14:53:01 +0600

    Removed

--- includes/assets/assets/Frontend-BBfArQFu.js
+++
@@ -1,7 +0,0 @@
-import { j as jsxRuntimeExports } from "./Frontend-otZikUsK.js";
-function Blur({ settings, imageUrl, title, subtitle, description, socialIcons }) {
-  return /* @__PURE__ */ jsxRuntimeExports.jsx(jsxRuntimeExports.Fragment, { children: /* @__PURE__ */ jsxRuntimeExports.jsx("h1", { children: "Ayan Blur Layout" }) });
-}
-export {
-  Blur as default
-};
