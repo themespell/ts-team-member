9af9f5bee0b4edb13363fdcdf8a35608d713327a
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-22 13:15:37 +0600

    Added Back to Admin Button

--- src/editor/components/Topbar.jsx
+++ src/editor/components/Topbar.jsx
@@ -6,11 +6,20 @@ function Topbar({ type }) {
         const action = `tsteam/${type}/update/settings`;
         editorFunction.getState().updateSettings(action);
     };
+    
+    const handleBacktoAdmin = () => {
+        const admin_url = `admin.php?page=tsteam-showcase`;
+        window.location.href = admin_url;
+    };
 
     return (
         <>
         <div className="flex justify-end tsteam__color--bg p-4">
-            <div className="flex justify-end w-2/6">
+            <div className="flex justify-end w-2/6 gap-2">
+                <TsButton 
+                    label="Back to Admin"
+                    onClick={handleBacktoAdmin}
+                />
                 <TsButton 
                     label="Publish"
                     onClick={handlePublishClick}
