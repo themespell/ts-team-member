5cc1deeec8096e0a230a255b3e875b281f549e51
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-22 13:17:45 +0600

    Removed

--- src/admin/states/admin-store.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'zustand'
-
-const useAdminStore = create((set) => ({
-    isOpen: false,
-    openModal: () => set({ isOpen: true }),
-    closeShowcaseModal: () => set({ isOpen: false }),
-}))
-
-export default useAdminStore;
