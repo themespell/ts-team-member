6a92ff8f6469d7b6fa122f8eaf744b87c160f98b
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-22 13:16:32 +0600

    Updated Modal Control

--- src/common/components/helper/TableNavItems.jsx
+++ src/common/components/helper/TableNavItems.jsx
@@ -1,14 +1,21 @@
-import useAdminStore from '../../../admin/states/admin-store.js';
+import commonStore from '../../states/commonStore.js';
 import { TsButton } from '../controls/tsControls.js';
 
 function TableNavItems({title}) {
-    const { isOpen, openModal, closeShowcaseModal } = useAdminStore();
+    const { saveSettings } = commonStore((state) => ({
+      saveSettings: state.saveSettings,
+    }));
+
+    const handleOpenModal = () => {
+      saveSettings('createModal', true);
+    };
+
     return (
       <div className="flex justify-between">
         <TsButton 
         label={title}
         prefix='Create'
-        onClick={openModal}
+        onClick={handleOpenModal}
         />
       </div>                
     );
