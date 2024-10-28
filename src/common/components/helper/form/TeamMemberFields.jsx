a0e4e5a2606c280c71ce3420f2d96dd09918d77c
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-10-28 16:59:48 +0600

    Updated

--- src/common/components/helper/form/TeamMemberFields.jsx
+++ src/common/components/helper/form/TeamMemberFields.jsx
@@ -3,17 +3,20 @@ import { Tabs } from 'antd';
 
 import TeamMemberBasic from './TeamMember/TeamMemberBasic';
 import TeamMemberProfile from './TeamMember/TeamMemberProfile';
-import { fetchData } from '../../../services/fetchData';
 import TeamMemberDetails from './TeamMember/TeamMemberDetails';
 import TeamMemberCustom from './TeamMember/TeamMemberCustom';
 
+import { fetchData } from '../../../services/fetchData';
+
 function TeamMemberFields({ form, post_id }) {
   const [memberImage, setMemberImage] = useState(null);
+  const [socialLinks, setSocialLinks] = useState(null);
 
   useEffect(() => {
     if (post_id) {
       fetchData(`tsteam/team_member/fetch/single`, (response) => {
         if (response.success && response.data) {
+
           form.setFieldsValue({
             member_name: response.data.title,
             member_designation: response.data.meta_data.designation,
@@ -21,8 +24,18 @@ function TeamMemberFields({ form, post_id }) {
             member_description: response.data.content,
             member_email: response.data.meta_data.email,
             member_phone: response.data.meta_data.phone,
+
+            member_telephone: response.data.meta_data.telephone,
+            member_experience: response.data.meta_data.experience,
+            member_company: response.data.meta_data.company,
+            member_location: response.data.meta_data.location,
+            member_website: response.data.meta_data.website,
+            member_resume: response.data.meta_data.resume,
+            member_hire: response.data.meta_data.hireLink,
+            member_donation: response.data.meta_data.donationLink,
           });
           setMemberImage(response.data.meta_data.image);
+          setSocialLinks(response.data.meta_data.socialLinks);
         } else {
           console.error('Failed to fetch showcase data.');
         }
@@ -44,7 +57,7 @@ function TeamMemberFields({ form, post_id }) {
     {
       key: '3',
       label: 'Profile Links',
-      children: <TeamMemberProfile form={form} />,
+      children: <TeamMemberProfile form={form} social_links={socialLinks} />,
     },
     // {
     //   key: '4',
