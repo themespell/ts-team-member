2dcdf8481b851d026d14343da48b986a97a5d93e
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-11-17 22:40:39 +0600

    Added Responsive to Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,22 @@ export default {
     './parts/**/*.html',
   ],
   theme: {
+    screens: {
+      'sm': '360px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   plugins: [
