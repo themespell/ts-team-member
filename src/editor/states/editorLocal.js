ef05af2ebacbb4bec0c40e4ce719f44b37eea6f1
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-11-18 22:37:12 +0600

    Editor Local Updated

--- src/editor/states/editorLocal.js
+++ src/editor/states/editorLocal.js
@@ -1,11 +1,28 @@
 import { create } from 'zustand';
 import editorHelper from "./editorHelper.js";
+import proLayouts from "../../pro_support/proLayouts.js";
+
+// Get layouts from Pro version
+const pro_layouts = proLayouts();
 
 // States Only For use in Editor
 const editorLocal = create((set) => ({
     isEditor: true,
     viewport: editorHelper.getViewport(window.innerWidth),
     setViewport: (newViewport) => set({ viewport: newViewport }),
+
+    availableLayouts: [
+        { label: 'Card', value: 'Card', type: 'free' },
+        { label: 'Overlay', value: 'Overlay', type: 'free' },
+        { label: 'Blur', value: 'Blur', type: 'free' },
+        { label: 'Avatar', value: 'Avatar', type: 'free' },
+        { label: 'HorizontalCard', value: 'HorizontalCard', type: 'free' },
+        ...pro_layouts,
+    ],
+    availableViews: [
+        { label: 'Static / Grid', value: 'grid', type: 'free' },
+        { label: 'Carousel', value: 'carousel', type: 'free' },
+    ],
 }));
 
 export default editorLocal;
