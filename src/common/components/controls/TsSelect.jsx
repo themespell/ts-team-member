d330fd74e9aa6f8a35911c40b41083a5f38829c8
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-11-18 22:38:00 +0600

    Improved Select Component

--- src/common/components/controls/TsSelect.jsx
+++ src/common/components/controls/TsSelect.jsx
@@ -1,8 +1,26 @@
 import { Select} from 'antd';
 import get from 'lodash/get';
+import editorStore from "../../../editor/states/editorStore.js";
+import editorFunction from '../../../editor/states/editorFunction';
 import globalSettings from '../../utils/globalSettings';
 
-function TsSelect({ label, value, options, onChange, mode }) {
+function TsSelect({ label, name, options, onChange, mode, output = 'value' }) {
+  const { saveSettings } = editorFunction();
+
+  const handleChange = (value) => {
+        if (onChange) {
+            onChange(value);
+        } else {
+            if ( output === 'value' ) {
+                saveSettings(name, value);
+            } else if ( output === 'object' ) {
+                const selectedOption = options.find(option => option.value === value);
+                const result = { ...selectedOption };
+                saveSettings(name, result);
+            }
+        }
+    };
+
   return (
     <div className="mb-4">
       {label && (
@@ -17,11 +35,11 @@ function TsSelect({ label, value, options, onChange, mode }) {
       )}
       <Select
         placeholder={`Select ${label}`}
-        value={value}
+        value={get(editorStore(), name)}
         style={{ 
           width: '100%',
         }}
-        onChange={onChange}
+        onChange={handleChange}
         options={options}
         mode={mode}
       />
