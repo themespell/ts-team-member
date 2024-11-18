d909d8ccf180193d3341369a3a00bdcd30bef2ea
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-11-18 22:36:41 +0600

    Added Pro Layout Check

---
+++ src/pro_support/proLayouts.js
@@ -0,0 +1,25 @@
+function proLayouts(selectedLayout) {
+    const isPro = tsteam_settings.is_pro
+
+    if (!isPro) {
+        return [];
+    }
+
+    else {
+        if (selectedLayout) {
+            const layout = window.tsTeamPro?.Layouts[selectedLayout].Frontend;
+            return layout;
+        }
+        else {
+            const layouts = window.tsTeamPro?.Layouts;
+            const layoutNames = Object.keys(layouts).map((layoutName) => ({
+                label: layoutName,
+                value: layoutName,
+            }));
+
+            return layoutNames;
+        }
+    }
+}
+
+export default proLayouts;
