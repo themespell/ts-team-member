eb427d9bda0e7a4bde4cf566c0b5462efaa2ba47
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-11-18 22:37:49 +0600

    Updated

--- src/editor/components/Sidebar/Sidebar.jsx
+++ src/editor/components/Sidebar/Sidebar.jsx
@@ -5,29 +5,29 @@ import AdvanceTab from "./Tabs/AdvanceTab";
 import { TsDrawer } from '../../../common/components/controls/tsControls';
 
 function Sidebar({ isOpen, onClose, layoutType }) {
-  const items = [
-    {
-      label: 'Content',
-      key: '1',
-      children: <ContentTab />,
-    },
-    {
-      label: 'Style',
-      key: '2',
-      children: <StyleTab layoutType={layoutType} />,
-    },
-    {
-      label: 'Advance',
-      key: '3',
-      children: <AdvanceTab />,
-    },
-  ];
+    const items = [
+        {
+            label: 'Content',
+            key: '1',
+            children: <ContentTab />,
+        },
+        {
+            label: 'Style',
+            key: '2',
+            children: <StyleTab layoutType={layoutType} />,
+        },
+        {
+            label: 'Advance',
+            key: '3',
+            children: <AdvanceTab />,
+        },
+    ];
 
-  return (
-    <TsDrawer label="Editor" isOpen={isOpen} onClose={onClose} position="left">
-      <Tabs type="card" items={items} size="large"/>
-    </TsDrawer>
-  );
+    return (
+        <TsDrawer label="Editor" isOpen={isOpen} onClose={onClose} position="left">
+            <Tabs type="card" items={items} size="large"/>
+        </TsDrawer>
+    );
 }
 
 export default Sidebar;
