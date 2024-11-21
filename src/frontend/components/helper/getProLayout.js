47afc74d8e0be3289a0cbc2a36b96131a2b71676
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-11-21 12:46:21 +0600

    Updated

--- src/frontend/components/helper/getProLayout.js
+++ src/frontend/components/helper/getProLayout.js
@@ -2,7 +2,7 @@ import proLayouts from "../../../pro_support/proLayouts.js";
 
 export function getProLayout(settings) {
     if (settings?.selectedLayout?.type === 'pro') {
-        const layout = proLayouts(settings.selectedLayout.value);
+        const layout = proLayouts(settings.selectedLayout?.value);
         return layout.Frontend;
     }
     return null;
