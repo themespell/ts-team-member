f74060acff0d71d406988232e2fc5d66d0b8769a
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-12-17 12:04:38 +0600

    Updated

---
+++ src/admin/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+function Dashboard() {
+
+    return (
+        <div className="min-h-fit flex">
+            {/* Main Content */}
+            <div className="flex-1">
+                Dashboard
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
