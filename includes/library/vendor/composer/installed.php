5cec793fde3267a2d3729481c7757d8eecb5a0ab
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2024-12-17 12:03:42 +0600

    Added Compose For Library & Freemius Support

---
+++ includes/library/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'themespell/tsteam',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '230491e29ac181b4ca7f2836b248475e02c22960',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'freemius/wordpress-sdk' => array(
+            'pretty_version' => '2.9.0',
+            'version' => '2.9.0.0',
+            'reference' => '56696717c5f0b7fb395618e88e1b592d1893e5dc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../freemius/wordpress-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'themespell/tsteam' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '230491e29ac181b4ca7f2836b248475e02c22960',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
