5f361ef10954b8acf7366c4f88c51a47deab19b0
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2025-01-15 15:39:15 +0600

    Topbar Updated

--- src/admin/components/Topbar.jsx
+++ src/admin/components/Topbar.jsx
@@ -4,15 +4,14 @@ import globalSettings from "../../common/utils/globalSettings";
 function Topbar({ title }) {
     const tsteamLogo = tsteam_settings.assets_path;
     const { menuitems, proLink, version } = globalSettings.topbar;
-    console.log(tsteamLogo)
 
     return (
         <div className="tsteam__color--bg w-full flex justify-center shadow-4xl">
-            <div className="w-5/6 tsteam__color--link py-4 flex items-center justify-between px-4">
+            <div className="w-4/6 tsteam__color--link py-4 flex items-center justify-between px-4">
                 {/* Left Section: Logo and Navigation */}
                 <div className="flex items-center space-x-8">
                     {/* Ts Team Logo */}
-                    <img src={`${tsteamLogo}/img/tsteam_icon.svg`} className="w-10 h-10"/>
+                    <img src={`${tsteamLogo}/img/tsteam_icon.svg`} className="tsteam__topbar-logo w-10 h-10"/>
 
                     {/* Navigation Links */}
                     <div className="flex space-x-4 gap-4 tsteam__topbar-nav mt-2">
@@ -38,9 +37,9 @@ function Topbar({ title }) {
 
                     {/* Icons */}
                     <div className="relative flex gap-4">
-                        <CircleHelp size={24} strokeWidth={1} className="tsteam__color--link" />
-                        <Bell size={24} strokeWidth={1} className="tsteam__color--link" />
-                        <UserRound size={24} strokeWidth={1} className="tsteam__color--link" />
+                        <a href="https://wordpress.org/support/plugin/ts-team-member" target="_blank"><CircleHelp size={24} strokeWidth={1.5} className="tsteam__color--link" /></a>
+                        <Bell size={24} strokeWidth={1.5} className="tsteam__color--link" />
+                        <a href="admin.php?page=tsteam-pro-account"><UserRound size={24} strokeWidth={1.5} className="tsteam__color--link"/></a>
                     </div>
                 </div>
             </div>
