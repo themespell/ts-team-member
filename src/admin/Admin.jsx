235eb1c408cadf3d662ad6b8e2d1baa3fc2d6ec1
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2025-02-03 18:25:46 +0600

    Uodated

--- src/admin/Admin.jsx
+++ src/admin/Admin.jsx
@@ -1,6 +1,7 @@
 import TeamShowcase from './components/TeamShowcase';
 import TeamMember from './components/TeamMember';
 import Dashboard from "./components/Dashboard.jsx";
+import Settings from "./components/Settings.jsx";
 import Topbar from './components/Topbar';
 
 const currentUrl = window.location.href;
@@ -8,6 +9,7 @@ const currentUrl = window.location.href;
 function AdminPanel() {
   const isTeamMemberPage = currentUrl.includes(`&path=team-member`);
   const isDashboardPage = currentUrl.includes(`&path=dashboard`);
+  const isSettingsPage = currentUrl.includes(`&path=settings`);
 
     if (isDashboardPage) {
         return (
@@ -17,6 +19,19 @@ function AdminPanel() {
                     <div className='flex justify-between gap-8 w-4/6'>
                         <div className='w-full'>
                             <Dashboard/>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    } else if (isSettingsPage) {
+        return (
+            <>
+                <Topbar title={'Settings'}/>
+                <div className='tsteam__admin--style overflow-x-auto w-full flex justify-center pt-12 pb-12'>
+                    <div className='flex justify-between gap-8 w-4/6'>
+                        <div className='w-full'>
+                            <Settings/>
                         </div>
                     </div>
                 </div>
