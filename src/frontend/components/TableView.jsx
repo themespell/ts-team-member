c27ac794cb138083d8eab5551cb31145104a3fe6
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2025-02-17 00:34:54 +0600

    Added Table View

---
+++ src/frontend/components/TableView.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import Layout from './layouts/Layout';
+import { getCommonStyles } from "./helper/commonStyle.js";
+import { getResponsiveStyles } from "./helper/responsiveStyles.js";
+import { getProLayout } from "./helper/getProLayout.js";
+
+function TableView({ team_members, settings, viewport, isEditor, Details }) {
+    const [ProLayoutComponent, setProLayoutComponent] = useState(null);
+    const commonStyles = getCommonStyles(settings);
+    const [responsiveStyles, setResponsiveStyles] = useState(
+        getResponsiveStyles(settings, viewport, isEditor)
+    );
+
+    useMemo(() => {
+        setProLayoutComponent(() => getProLayout(settings));
+    }, [settings?.selectedLayout?.type, settings?.selectedLayout?.value]);
+
+    useEffect(() => {
+        const updateResponsiveStyles = () => {
+            setResponsiveStyles(getResponsiveStyles(settings, viewport, isEditor));
+        };
+
+        if (isEditor) {
+            updateResponsiveStyles();
+        } else {
+            updateResponsiveStyles();
+            window.addEventListener('resize', updateResponsiveStyles);
+            return () => {
+                window.removeEventListener('resize', updateResponsiveStyles);
+            };
+        }
+    }, [settings, isEditor, viewport]);
+
+    return (
+        <div
+            className="tsteam-table-container"
+            style={{
+                ...commonStyles,
+                ...responsiveStyles,
+            }}
+        >
+            {ProLayoutComponent ? (
+                <ProLayoutComponent
+                    settings={settings}
+                    team_members={team_members}
+                    Details={Details}
+                />
+            ) : (
+                <Layout
+                    settings={settings}
+                    layoutType={settings.selectedLayout.value}
+                    team_members={team_members}
+                    Details={Details}
+                />
+            )}
+        </div>
+    );
+}
+
+export default TableView;
