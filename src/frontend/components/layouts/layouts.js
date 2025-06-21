8a93bdb874cb0929c502bb0551cfa38948c5ac7b
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2025-06-21 12:18:09 +0600

    Updated

--- src/frontend/components/layouts/layouts.js
+++ src/frontend/components/layouts/layouts.js
@@ -2,6 +2,5 @@
 import * as Card from './Card/Editor.jsx';
 import * as HorizontalCard from './HorizontalCard/Editor.jsx';
 import * as OverlayCard from './OverlayCard/Editor.jsx';
-import * as StandardTable from './StandardTable/Editor.jsx';
 
-export { Card,HorizontalCard,OverlayCard,StandardTable};
+export { Card,HorizontalCard,OverlayCard};
