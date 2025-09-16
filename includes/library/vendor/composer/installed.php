f776cf0608c5f7f6f43d13e79a5f8e12c0097b3d
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2025-09-16 18:54:56 +0600

    Freemius Added Through Composer

---
+++ includes/library/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'themespell/tsteam',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '46326ce4b64acd7ec85a9d62d66f7c98a0ef7d52',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'freemius/wordpress-sdk' => array(
+            'pretty_version' => '2.12.1',
+            'version' => '2.12.1.0',
+            'reference' => 'fedeba08eb3e62af5f84bd6d602f98636467cffc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../freemius/wordpress-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'themespell/tsteam' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '46326ce4b64acd7ec85a9d62d66f7c98a0ef7d52',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
