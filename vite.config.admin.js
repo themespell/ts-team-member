c60ea54802af8a55b003ea96f62cb4c1967ed8e2
Asaduzzaman Abir <24668412+anonnaabir00@users.noreply.github.com>
2025-09-25 19:27:55 +0600

    Revert to minify

--- vite.config.admin.js
+++ vite.config.admin.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     build: {
-        minify: false,
+        minify: true,
         manifest: false,
         rollupOptions: {
             input: path.resolve(__dirname, 'src/main.jsx'), // Admin entry point
